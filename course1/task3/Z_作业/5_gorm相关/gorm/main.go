608ab0ef5de4007dec60140fe50ccbf2df0d5d38
周泽 <ze.zhou@jimuitech.com>
2025-05-15 08:21:06 +0000

    add all

--- course1/task3/Z_作业/5_gorm相关/gorm/main.go
+++ course1/task3/Z_作业/5_gorm相关/gorm/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func main() {
 
@@ -12,9 +15,122 @@ func main() {
 		log.Println("AutoMigrate error. ", err)
 	}
 
+	// 创建数据
+	//createData()
+
 	//使用Gorm查询某个用户发布的所有文章及其对应的评论信息
 
-	//使用Gorm查询评论数量最多的文章信息
+	// select p.* from users u left join posts p on u.id = p.user_id where u.Name = ?;
+	var users []User
+	DB.Preload("Posts").Preload("Comments").Where("id=?", "2").Find(&users)
+	for _, u := range users {
+		log.Println("Comments", u.Comments)
+		log.Println("Posts", u.Posts)
+	}
 
-	closeDB()
+	log.Println("使用Gorm查询评论数量最多的文章信息")
+
+	//使用Gorm查询评论数量最多的文章信息
+	log.Println("使用Gorm查询评论数量最多的文章信息")
+	var posts []Result
+	DB.Model(&Post{}).Select("posts.*, count(comments.id) as comment_count").
+		Joins("left join comments on posts.id = comments.post_id").
+		Group("posts.id").
+		Order("comment_count desc").
+		Limit(1).
+		Scan(&posts)
+	for _, post := range posts {
+		log.Println("post comment_count: ", post.CommentCount)
+	}
+
+	// 为 Post 模型添加一个钩子函数，在文章创建时自动更新用户的文章数量统计字段
+	//createPost()
+
+	// 为 Comment 模型添加一个钩子函数，在评论删除时检查文章的评论数量，如果评论数量为 0，则更新文章的评论状态为 "无评论"
+	deleteComment()
+
+	//closeDB()
+
+	time.Sleep(10 * time.Second)
+}
+
+type Result struct {
+	Post
+	CommentCount int64
+}
+
+func createData() {
+	user := User{
+		Name:       "小明",
+		Age:        12,
+		LastUpdate: time.Now(),
+		Posts: []Post{
+			{
+				Title:      "小明的第一篇文章",
+				Content:    "小明的第一篇文章内容",
+				LastUpdate: time.Now(),
+			},
+			{
+				Title:      "小明的第二篇文章",
+				Content:    "小明的第二篇文章内容",
+				LastUpdate: time.Now(),
+			},
+		},
+		Comments: []Comment{},
+	}
+
+	DB.Create(&user)
+
+	user2 := User{
+		Name:       "小王",
+		Age:        15,
+		LastUpdate: time.Now(),
+		Posts: []Post{
+			{
+				Title:      "小王的第一篇文章",
+				Content:    "小王的第一篇文章内容",
+				LastUpdate: time.Now(),
+			},
+			{
+				Title:      "小王的第二篇文章",
+				Content:    "小王的第二篇文章内容",
+				LastUpdate: time.Now(),
+			},
+		},
+		Comments: []Comment{
+			{
+				PostId:     user.Posts[0].Id,
+				Content:    "小明的第一篇文章的评论",
+				LastUpdate: time.Now(),
+			},
+		},
+	}
+
+	DB.Create(&user2)
+}
+
+func createPost() {
+	DB.Create(&Post{
+		UserId:     3,
+		Title:      "小明的第三篇文章",
+		Content:    "小明的第三篇文章内容",
+		LastUpdate: time.Now(),
+	})
+}
+
+func deleteComment() {
+	var comment Comment
+	var commentId int64 = 3
+	if err := DB.First(&comment, commentId).Error; err != nil {
+		log.Println("Get comment error. ", err)
+		return
+	}
+
+	result := DB.Delete(&comment, commentId)
+	if result.Error != nil {
+		log.Println("Delete comment error. ", result.Error)
+	} else {
+		log.Println("Delete comment success.")
+	}
+	log.Println("rows affected:", result.RowsAffected)
 }
--- course1/task3/Z_作业/5_gorm相关/gorm/models.go
+++ course1/task3/Z_作业/5_gorm相关/gorm/models.go
@@ -1,11 +1,16 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
 	Id         int64     `json:"id" gorm:"primaryKey;autoIncrement;column:id"`
 	Name       string    `json:"name" gorm:"not null;column:name"`
 	Age        int       `json:"age" gorm:"not null;column:age"`
+	PostCount  int64     `json:"post_count" gorm:"default:0;column:post_count"`
 	LastUpdate time.Time `json:"last_update" gorm:"not null;column:last_update"`
 
 	Posts    []Post    `json:"posts" gorm:"foreignKey:UserId;references:Id"`
@@ -13,11 +18,12 @@ type User struct {
 }
 
 type Post struct {
-	Id         int64     `json:"id" gorm:"primaryKey;autoIncrement;column:id"`
-	UserId     int64     `json:"user_id" gorm:"not null;column:user_id;index"`
-	Title      string    `json:"title" gorm:"not null;column:title"`
-	Content    string    `json:"content" gorm:"not null;column:content"`
-	LastUpdate time.Time `json:"last_update" gorm:"not null;column:last_update"`
+	Id            int64     `json:"id" gorm:"primaryKey;autoIncrement;column:id"`
+	UserId        int64     `json:"user_id" gorm:"not null;column:user_id;index"`
+	Title         string    `json:"title" gorm:"not null;column:title"`
+	Content       string    `json:"content" gorm:"not null;column:content"`
+	LastUpdate    time.Time `json:"last_update" gorm:"not null;column:last_update"`
+	CommentStatus string    `json:"comment_status" gorm:"default null;column:comment_status"`
 
 	Comments []Comment `json:"comments" gorm:"foreignKey:PostId;references:Id"`
 }
@@ -28,4 +34,36 @@ type Comment struct {
 	PostId     int64     `json:"post_id" gorm:"not null;column:post_id;index"`
 	Content    string    `json:"content" gorm:"not null;column:content"`
 	LastUpdate time.Time `json:"last_update" gorm:"not null;column:last_update"`
+}
+
+// 为 Post 模型添加一个钩子函数，在文章创建时自动更新用户的文章数量统计字段
+func (p *Post) AfterCreate(tx *gorm.DB) (err error) {
+	fmt.Println("AfterCreate hook triggered")
+	return tx.Model(&User{}).
+		Where("id = ?", p.UserId).
+		UpdateColumn("post_count", gorm.Expr("COALESCE(post_count, 0) + ?", 1)).
+		Error
+}
+
+// 为 Comment 模型添加一个钩子函数，在评论删除时检查文章的评论数量，如果评论数量为 0，则更新文章的评论状态为 "无评论"
+func (c *Comment) AfterDelete(tx *gorm.DB) (err error) {
+	fmt.Println("AfterDelete hook triggered")
+	var post Post
+	if err := tx.Model(&Post{}).Where("id = ?", c.PostId).First(&post).Error; err != nil {
+		return err
+	}
+	var count int64
+	if err := tx.Model(&Comment{}).Where("post_id = ?", c.PostId).Count(&count).Error; err != nil {
+		return err
+	}
+	if count == 0 {
+		return tx.Model(&post).UpdateColumn("comment_status", "无评论").Error
+	}
+	return nil
+}
+
+func (c *Comment) BeforeDelete(tx *gorm.DB) (err error) {
+	// 把PostId等信息存到钩子自身的字段里，确保AfterDelete可用
+	// 这里 c.PostId 就不会是空的，因为 BeforeDelete 是删除前调用的
+	return nil
 }
